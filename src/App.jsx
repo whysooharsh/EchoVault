a6f2eb908ae328af88fb5de4db0949607e19fd84
HARSH SHARMA <harsh.sharma.8986@gmail.com>
2025-03-18 23:19:32 +0530

    bruh

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import Homepage from "./pages/homepage";
 import Aboutus from "./pages/aboutus";
-import Navbar from "./components/navbar";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./pages/login";
+import Layout from "./layout";
+import Signup from "./pages/signup";
 
 
 
 function App(){
   return ( 
     <Router>
-      <Navbar />
+      <Layout />
       <Routes>
       <Route path = "/" element = {<Homepage />}/>
       <Route path = "/aboutus" element = {<Aboutus />}/>
-    
+      <Route path = "/login" element = {<Login />}/>
+      <Route path = "/signup" element = {<Signup />}/>
+
       </Routes>
     </Router>
   );
--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,8 +1,108 @@
-function Signup(){
+function signup() {
     return (
-        <div>
+      <div
+        style={{
+          margin: "0px",
+          padding: "0px",
+          minHeight: "100vh",
+          width: "100vw",
+          backgroundColor: "wheat",
+          display: "flex",
+          flexDirection: "column",
+          paddingTop: "60px",
+          overflow : "hidden",
+        }}
+      >
+        <style>{`
+              * {
+                margin: 0;
+                padding: 0;
+                box-sizing: border-box;
+                font-family: Fredoka;
+              }
+              body {
+                background-color: wheat;
+              }
+            `}</style>
+  
+        <div
+          style={{
+            padding: "5rem",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "1.4rem",
+              padding: "1.2rem",
+            }}
+          >
+            Create an account
+          </div>
+          <div
+            style={{
+              height: "480px",
+              width: "25%",
+              padding: "0 40px 40px 40px",
+              border: "1px solid black",
+              borderRadius: "0.8rem",
+              boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+              backdropFilter: "blur(10px)", 
+              backgroundColor: "transparent", 
+              overflow : "hidden",
+            }}
+          >
+            <form onSubmit={""}>
+              <input
+                style={{
+                  marginTop : "1.2rem",
+                  padding: "1rem 2rem",
+                  width: "100%",
+                  borderRadius: "0.4rem",
+                  border: "1px solid #ccc",
+                  overflow : "hidden",
+                  background : "transparent",
+                }}
+                type="text"
+                name="Email"
+                placeholder="Email address*"
+                required
+              />
+              <input
+                style={{
+                  marginTop : "1.2rem",
+                  padding: "1rem 2rem",
+                  width: "100%",
+                  borderRadius: "0.4rem",
+                  border: "1px solid #ccc",
+                  overflow : "hidden",
+                  background : "transparent",
+                }}
+                type="password"
+                name="password"
+                placeholder="Password*"
+                required
+              />
+              <button style={{
+                marginTop : "1.2rem",
+                padding: "1rem 2rem",
+                width: "100%",
+                borderRadius: "0.4rem",
+                border: "1px solid #ccc",
+                overflow : "hidden",
+                background : "red",
 
+
+              }}>SignUp</button>
+            </form>
+          </div>
         </div>
-    )
-}
-export default Signup;
+      </div>
+    );
+  }
+  
+  export default signup;
+  
